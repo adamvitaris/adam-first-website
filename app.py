a18358c90e4afb06048753e22327412295a75d21
adamvitaris <126414868+adamvitaris@users.noreply.github.com>
2023-02-28 08:32:34 +0000

    Added dynamic data and api route.

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PLACES = [
+  {
+    'id': 1,
+    'city':'Barcelona',
+    'location':'Spain',
+    'program':'City Tour'
+  },
+  {
+    'id': 2,
+    'city':'Crete',
+    'location':'Greece',
+    'program':'Swimming'
+  },
+   {
+    'id': 3,
+    'city':'Bohinj',
+    'location':'Slovenia',
+    'program':'Hiking'
+  }
+]
+
 @app.route("/")
 def hello_wordl():
-  return render_template('home.html')
+  return render_template('home.html',
+                        places=PLACES)
+
+@app.route("/api/places")
+def list_places():
+  return jsonify(PLACES)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
